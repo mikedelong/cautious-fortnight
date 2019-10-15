d1299072b78a0fe99116a1702790ab5994dd0512
mikedelong <mikedelong@outlook.com>
2019-10-15 09:43:33 -0400

    feature: read in the source data

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import nltk
+import numpy as np
+import random
+import string
+
 if __name__ == '__main__':
-    response = ''
-    while response not in {'quit'}:
-        response = input('Yes? ')
-        print(response)
+
+    do_basic_loop = False
+    if do_basic_loop:
+        response = ''
+        while response not in {'quit'}:
+            response = input('Yes? ')
+            print(response)
+
+    input_file = './data/chatbot.txt'
+    errors_ = 'ignore'
+    with open(input_file, 'r', errors=errors_) as input_fp:
+        raw_text = input_fp.read()
