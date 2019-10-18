84ebd68fe22c2cc0db99a41fef62b340b97a3168
mikedelong <mikedelong@outlook.com>
2019-10-18 10:55:10 -0400

    refactor: contract the parameter list to the vectorizer to a single line; also, remove some redundant code from the success branch

--- main.py
+++ main.py
@@ -29,12 +29,11 @@ def greeting(sentence):
 STOP_WORDS = {word for word in {'ha', 'le', 'u', 'wa'}.union(ENGLISH_STOP_WORDS)}
 
 choice_algorithm = {'highest': 0, 'probabilistically': 1}['probabilistically']
+
+
 def respond(arg_user_response, arg_tokens):
     arg_tokens.append(arg_user_response)
-    vectorizer = TfidfVectorizer(
-        lowercase=False,
-        tokenizer=normalize,
-        stop_words=STOP_WORDS, )
+    vectorizer = TfidfVectorizer(lowercase=False, tokenizer=normalize, stop_words=STOP_WORDS, )
     tfidf = vectorizer.fit_transform(arg_tokens)
     similarity = cosine_similarity(tfidf[-1], tfidf)
     if np.count_nonzero(similarity.flatten()) == 1:
@@ -42,8 +41,7 @@ def respond(arg_user_response, arg_tokens):
     else:
         t = np.random.choice(range(len(similarity[0]) - 1), 1, p=(similarity[0][:-1] / np.sum(similarity[0][:-1])))[0]
         index = similarity.argsort()[0][-2] if choice_algorithm == 0 else t
-        result = arg_tokens[index] if np.count_nonzero(similarity.flatten()) > 1 else 'sorry please try again.'
-        return result
+        return arg_tokens[index]
 
 
 GREETING_INPUTS = ('hello', 'hi', 'greetings', 'sup', 'what\'s up', 'hey',)
