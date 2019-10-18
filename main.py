d9653e4e845c56c1986bf8719803e33151a8ad42
mikedelong <mikedelong@outlook.com>
2019-10-18 11:01:03 -0400

    cleanup: get all the global variables together in one section, in order cleanup: remove an unnecessary local variable

--- main.py
+++ main.py
@@ -26,9 +26,10 @@ def greeting(sentence):
             return random.choice(GREETING_RESPONSES)
 
 
+GREETING_INPUTS = ('hello', 'hi', 'greetings', 'sup', 'what\'s up', 'hey',)
+GREETING_RESPONSES = ['hi', 'hey', '*nods*', 'hi there', 'hello', 'hooray!']
+INDEX_CHOICE = {'highest': 0, 'probabilistically': 1}['probabilistically']
 STOP_WORDS = {word for word in {'ha', 'le', 'u', 'wa'}.union(ENGLISH_STOP_WORDS)}
-
-choice_algorithm = {'highest': 0, 'probabilistically': 1}['probabilistically']
 
 
 def respond(arg_user_response, arg_tokens):
@@ -39,16 +40,12 @@ def respond(arg_user_response, arg_tokens):
     if np.count_nonzero(similarity.flatten()) == 1:
         return 'sorry please try again.'
     else:
-        index = similarity.argsort()[0][-2] if choice_algorithm == 0 else \
+        index = similarity.argsort()[0][-2] if INDEX_CHOICE == 0 else \
             np.random.choice(range(len(similarity[0]) - 1), 1, p=(similarity[0][:-1] / np.sum(similarity[0][:-1])))[0]
         return arg_tokens[index]
 
 
-GREETING_INPUTS = ('hello', 'hi', 'greetings', 'sup', 'what\'s up', 'hey',)
-GREETING_RESPONSES = ['hi', 'hey', '*nods*', 'hi there', 'hello', 'hooray!']
-
 if __name__ == '__main__':
-
     lemmer = nltk.stem.WordNetLemmatizer()
     do_basic_loop = False
     if do_basic_loop:
@@ -60,10 +57,8 @@ if __name__ == '__main__':
     input_file = './data/chatbot.txt'
     errors_ = 'ignore'
     with open(input_file, 'r', errors=errors_) as input_fp:
-        raw_text = input_fp.read()
+        text = input_fp.read()
 
-    # text = raw_text.lower()
-    text = raw_text
     nltk.download('punkt')  # first-time use only
     nltk.download('wordnet')  # first-time use only
     sentences = nltk.sent_tokenize(text)  # converts to list of sentences
