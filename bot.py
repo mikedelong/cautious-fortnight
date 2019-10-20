87ef773f2bc1851b948cca5a63ed0b6f80ef71f0
mikedelong <mikedelong@outlook.com>
2019-10-20 09:42:31 -0400

    feature: introduce main/cutout code; move static data out of main

--- bot.py
+++ bot.py
@@ -4,21 +4,22 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-bot = ChatBot(name='bot', read_only=True,
-              logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
-
 small = ['hi there!', 'hi!', 'how do?', 'how you?', 'cool.', 'fine, you?', 'extra cool.', 'okay',
          'glad to hear it.', 'awesome', 'excellent', 'not so good', 'sorry to hear that.', 'what\'s your name?',
          'i\'m bot. ask me a math question, please.']
 math0 = ['pythagorean theorem', 'a squared plus b squared equals c squared.']
 math1 = ['law of cosines', 'c**2 = a**2 + b**2 - 2 * a * b * cos(gamma)']
 
-list_trainer = ListTrainer(chatbot=bot)
-for item in (small, math0, math1):
-    list_trainer.train(item)
+if __name__ == '__main__':
+    bot = ChatBot(name='bot', read_only=True,
+                  logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
 
-response = None
-while response != 'quit':
-    response = input('?: ')
-    if response != 'quit':
-        print(bot.get_response(response))
+    list_trainer = ListTrainer(chatbot=bot)
+    for item in (small, math0, math1):
+        list_trainer.train(item)
+
+    response = None
+    while response != 'quit':
+        response = input('?: ')
+        if response != 'quit':
+            print(bot.get_response(response))
