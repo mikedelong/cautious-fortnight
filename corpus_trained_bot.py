f14750284784d025029d9b6792929ad590c2f825
mikedelong <mikedelong@outlook.com>
2019-10-22 10:06:27 -0400

    refactor: we don't really need a Trainer instance; we just need it to train the bot

--- corpus_trained_bot.py
+++ corpus_trained_bot.py
@@ -4,11 +4,7 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 if __name__ == '__main__':
     chatbot = ChatBot(name='house')
 
-    # Create a new trainer for the chatbot
-    trainer = ChatterBotCorpusTrainer(chatbot)
-
-    # Train the chatbot based on the english corpus
-    trainer.train('chatterbot.corpus.english')
+    ChatterBotCorpusTrainer(chatbot).train('chatterbot.corpus.english')
 
     # Get a response to an input statement
     for _ in range(10):
