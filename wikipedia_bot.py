647a01558c22dd0601f82eecc85a0407d6555ac3
mikedelong <mikedelong@outlook.com>
2019-10-23 09:01:51 -0400

    feature: follow links to build a more robust corpus

--- wikipedia_bot.py
+++ wikipedia_bot.py
@@ -13,13 +13,21 @@ if __name__ == '__main__':
     download('wordnet')  # first-time use only
     lemmer = WordNetLemmatizer()
 
-    random_title = wikipedia.random()
-    print('{}'.format(random_title))
-    page = wikipedia.page(title=random_title)
-    print('{}'.format(page.content))
-    text = ' '.join([item for item in page.content.split('\n') if '==' not in item and len(item) > 1])
-    print(text)
-    sentences = sent_tokenize(text)  # converts to list of sentences
+    sentences = list()
+    count = 0
+    while count < 10:
+        random_title = wikipedia.random()
+        print('{}'.format(random_title))
+        if not random_title.startswith('List'):
+            count += 1
+            page = wikipedia.page(title=random_title)
+            print('{}'.format(page.content))
+            text = ' '.join([item for item in page.content.split('\n') if '==' not in item and len(item) > 1])
+            print(text)
+            new_sentences = sent_tokenize(text)
+            if 'is' in new_sentences[0] or 'was' in new_sentences[0]:
+                print(new_sentences[0])
+            sentences = sentences + new_sentences
     print('sentence count: {}'.format(len(sentences)))
 
     flag = True
