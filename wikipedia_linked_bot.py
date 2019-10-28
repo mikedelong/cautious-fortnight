d2aba24f1bf8827be18bd150e4f55c33d57a3e81
mikedelong <mikedelong@outlook.com>
2019-10-28 09:48:04 -0400

    feature: when we encounter a disambiguation error just pick one of the available options at random and proceed

--- wikipedia_linked_bot.py
+++ wikipedia_linked_bot.py
@@ -30,7 +30,7 @@ def get_sentences(page_name):
     # todo find a way to recover by picking one of the available choices
     except DisambiguationError as disambiguation_error:
         print('options are {}'.format(disambiguation_error.options))
-        return list()
+        return get_sentences(choice(disambiguation_error.options))
     content = local_page.content
     lines = [line for line in content.split('\n') if len(line) > 0]
     headings = [index for index, line in enumerate(lines) if line.startswith('==') or index == 0] + [len(lines)]
