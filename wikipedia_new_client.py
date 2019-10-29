e69d26fdcb8ceb96ca22d7efe462abe1a28a761c
mikedelong <mikedelong@outlook.com>
2019-10-29 10:38:53 -0400

    feature: we don't have access to a random page through this API, so we will need to rethink

--- wikipedia_new_client.py
+++ wikipedia_new_client.py
@@ -1,5 +1,11 @@
 from wikipediaapi import Wikipedia
 
 if __name__ == '__main__':
-    client = Wikipedia('en')
-    page = client.page('Chatbot')
+    client = Wikipedia(language='en')
+    name = 'Chatbot'
+    page = client.page(name)
+    if page.exists():
+        print('page [{}] exists'.format(name))
+    else:
+        print('page [{}] does not exist'.format(name))
+        quit(1)
