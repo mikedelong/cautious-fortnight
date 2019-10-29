9ed7c19823dc98cd8c858294e38740956baa2d2c
mikedelong <mikedelong@outlook.com>
2019-10-29 11:04:27 -0400

    feature: first working version to extract text on a per-section basis

--- wikipedia_new_client.py
+++ wikipedia_new_client.py
@@ -1,6 +1,25 @@
 from nltk import download
+from nltk import sent_tokenize
 from nltk.stem import WordNetLemmatizer
 from wikipediaapi import Wikipedia
+
+UNWANTED_HEADINGS = {'External links', 'References', 'See also', }
+
+
+def fix_period_splice(arg):
+    for index, character in enumerate(arg):
+        if character == '.' and 0 < index < len(arg) - 1:
+            if arg[index - 1].islower() and arg[index + 1].isupper():
+                result = ''.join([arg[:index], '. ', arg[index:]])
+                return fix_period_splice(result)
+    return arg
+
+
+def get_sentences(arg_page):
+    content = ''.join([section.text for section in arg_page.sections if section.title not in UNWANTED_HEADINGS])
+    local_text = fix_period_splice(content)
+    return sent_tokenize(local_text)
+
 
 if __name__ == '__main__':
     for package in ['punkt', 'wordnet']:
@@ -15,3 +34,5 @@ if __name__ == '__main__':
     else:
         print('page [{}] does not exist'.format(name))
         quit(1)
+    sentences = get_sentences(page)
+    print(sentences)
