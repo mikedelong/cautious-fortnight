f19d025044762ead2828daec736bb2dccd4aa2cc
mikedelong <mikedelong@outlook.com>
2019-10-29 10:41:09 -0400

    feature: introduce the lemmatizer; also download the NLTK packages as needed.

--- wikipedia_new_client.py
+++ wikipedia_new_client.py
@@ -1,6 +1,12 @@
+from nltk import download
+from nltk.stem import WordNetLemmatizer
 from wikipediaapi import Wikipedia
 
 if __name__ == '__main__':
+    for package in ['punkt', 'wordnet']:
+        download(package)
+    lemmer = WordNetLemmatizer()
+
     client = Wikipedia(language='en')
     name = 'Chatbot'
     page = client.page(name)
