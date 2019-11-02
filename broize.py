84185cae4fc386eaad19d77f6f7a7b3da521b4af
mikedelong <mikedelong@outlook.com>
2019-11-02 08:23:38 -0400

    cleanup: ongoing cleanup

--- broize.py
+++ broize.py
@@ -129,10 +129,10 @@ def find_adjective(sent):
 def construct_response(pronoun, noun, verb):
     """No special cases matched, so we're going to try to construct a full sentence that uses as much
     of the user's input as possible"""
-    resp = []
+    response = list()
 
     if pronoun:
-        resp.append(pronoun)
+        response.append(pronoun)
 
     # We always respond in the present tense, and the pronoun will always either be a pass-through
     # from the user, or 'you' or 'I', in which case we might need to change the tense for some
@@ -140,19 +140,18 @@ def construct_response(pronoun, noun, verb):
     if verb:
         verb_word = verb[0]
         if verb_word in ('be', 'am', 'is', "'m"):  # This would be an excellent place to use lemmas!
-            if pronoun.lower() == 'you':
+            if pronoun in {'you', 'You'}:
                 # The bot will always tell the person they aren't whatever they said they were
-                resp.append('aren\'t really')
+                response.append('aren\'t really')
             else:
-                resp.append(verb_word)
+                response.append(verb_word)
     if noun:
         pronoun = 'an' if starts_with_vowel(noun) else 'a'
-        resp.append(pronoun + ' ' + noun)
+        response.append(pronoun + ' ' + noun)
 
-    resp.append(random.choice(('tho', 'bro', 'lol', 'bruh', 'smh', '')))
+    response.append(random.choice(('tho', 'bro', 'lol', 'bruh', 'smh', '')))
 
-    return " ".join(resp)
-
+    return ' '.join(response)
 
 # end
 
