235b18e41b4af1cdd056a4244c06259e80e606b8
mikedelong <mikedelong@outlook.com>
2019-11-04 11:07:59 -0500

    feature: code that kind of works, with all the API updates fixed. Not yet really responsive.

--- demo_deep_pavlov.py
+++ demo_deep_pavlov.py
@@ -1,7 +1,11 @@
-# from deeppavlov.core.agent import Agent
-# from deeppavlov.agents.processors.default_rich_content_processor import DefaultRichContentWrapper
+from deeppavlov.agents.default_agent.default_agent import DefaultAgent
+from deeppavlov.agents.processors.highest_confidence_selector import HighestConfidenceSelector
 from deeppavlov.skills.pattern_matching_skill import PatternMatchingSkill
 
-bye = PatternMatchingSkill(['Goodbye world!', 'See you around'], patterns=["bye", "ciao", "see you"])
-hello = PatternMatchingSkill(responses=['Hello world!'], patterns=["hi", "hello", "good day"])
-fallback = PatternMatchingSkill(["I don't understand, sorry"])
+if __name__ == '__main__':
+    bye = PatternMatchingSkill(['Goodbye world!', 'See you around'], patterns=['bye', 'ciao', 'see you'])
+    hello = PatternMatchingSkill(responses=['Hello world!'], patterns=['good day', 'hello', 'hi'])
+    fallback = PatternMatchingSkill(['I\'m sorry; I don\'t understand.'])
+    agent = DefaultAgent([hello, bye, fallback], skills_selector=HighestConfidenceSelector())
+    agent(['Hello', 'Bye', 'Or not'])
+    print(agent.history)
