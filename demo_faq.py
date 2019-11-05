ecd9f2ceac615111c6693d5cd2d468d4c82d84c1
mikedelong <mikedelong@outlook.com>
2019-11-05 12:03:13 -0500

    feature: let's use the local JSON configuration file instead of the one in the configs package

--- demo_faq.py
+++ demo_faq.py
@@ -1,8 +1,7 @@
-from deeppavlov import configs
 from deeppavlov.core.commands.infer import build_model
 
 if __name__ == '__main__':
-    config_ = configs.faq.tfidf_logreg_en_faq
+    config_ = './tfidf_logreg_en_faq.json'
     model = build_model(config=config_, load_trained=True)
     result = model(['What time is it now?'])
     print(result)
