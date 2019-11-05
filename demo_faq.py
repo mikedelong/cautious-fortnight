898fd98ba983253b28314970c083626752b61b0c
mikedelong <mikedelong@outlook.com>
2019-11-05 11:45:35 -0500

    refactor: separate out the configuration as a variable

--- demo_faq.py
+++ demo_faq.py
@@ -2,5 +2,7 @@ from deeppavlov import configs
 from deeppavlov.core.commands.infer import build_model
 
 if __name__ == '__main__':
-    model = build_model(configs.faq.tfidf_logreg_en_faq, load_trained=True)
-    print(model)
+    config_ = configs.faq.tfidf_logreg_en_faq
+    model = build_model(config=config_, load_trained=True)
+    result = model(['What time is it now?'])
+    print(result)
