8cca36f6dc2048a920b3a6de54103735a42bfe77
mikedelong <mikedelong@outlook.com>
2019-11-06 10:12:29 -0500

    feature: present the question and answer in a more readable format

--- demo_faq.py
+++ demo_faq.py
@@ -3,5 +3,6 @@ from deeppavlov.core.commands.infer import build_model
 if __name__ == '__main__':
     config_ = './tfidf_logreg_en_faq.json'
     model = build_model(config=config_, download=True, load_trained=True, )
-    result = model(['What time is it now?'])
-    print(result)
+    question = 'What time is it now?'
+    result = model([question])
+    print('Q: {} A: {}'.format(question, result[0][0]))
