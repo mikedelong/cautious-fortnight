f85020229fa819442277065db84b53cafe448d77
mikedelong <mikedelong@outlook.com>
2019-11-06 10:09:20 -0500

    feature: first working version; produces poor results.

--- demo_faq.py
+++ demo_faq.py
@@ -2,6 +2,6 @@ from deeppavlov.core.commands.infer import build_model
 
 if __name__ == '__main__':
     config_ = './tfidf_logreg_en_faq.json'
-    model = build_model(config=config_, load_trained=True)
+    model = build_model(config=config_, download=True, load_trained=True, )
     result = model(['What time is it now?'])
     print(result)
