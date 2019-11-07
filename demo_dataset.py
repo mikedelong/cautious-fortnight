413b8430815775df89ee69788162d09a81fe5c58
mikedelong <mikedelong@outlook.com>
2019-11-07 12:39:12 -0500

    feature: introduce basic_config cleanup: single not double quotes

--- demo_dataset.py
+++ demo_dataset.py
@@ -31,116 +31,116 @@ class MyDialogDatasetIterator(DataLearningIterator):
 
 
 dslotfill = {
-    "dataset_reader": {
-        "class_name": "dstc2_reader",
-        "data_path": "{DATA_PATH}"
+    'dataset_reader': {
+        'class_name': 'dstc2_reader',
+        'data_path': '{DATA_PATH}'
     },
-    "dataset_iterator": {
-        "class_name": "dstc2_ner_iterator",
-        "slot_values_path": "{SLOT_VALS_PATH}"
+    'dataset_iterator': {
+        'class_name': 'dstc2_ner_iterator',
+        'slot_values_path': '{SLOT_VALS_PATH}'
     },
-    "chainer": {
-        "in": ["x"],
-        "in_y": ["y"],
-        "pipe": [
+    'chainer': {
+        'in': ['x'],
+        'in_y': ['y'],
+        'pipe': [
             {
-                "in": ["x"],
-                "class_name": "lazy_tokenizer",
-                "out": ["x_tokens"]
+                'in': ['x'],
+                'class_name': 'lazy_tokenizer',
+                'out': ['x_tokens']
             },
             {
-                "in": ["x_tokens"],
-                "config_path": "{NER_CONFIG_PATH}",
-                "out": ["x_tokens", "tags"]
+                'in': ['x_tokens'],
+                'config_path': '{NER_CONFIG_PATH}',
+                'out': ['x_tokens', 'tags']
             },
 
             {
-                "in": ["x_tokens", "tags"],
-                "class_name": "dstc_slotfilling",
-                "threshold": 0.8,
-                "save_path": "{MODEL_PATH}/model",
-                "load_path": "{MODEL_PATH}/model",
-                "out": ["slots"]
+                'in': ['x_tokens', 'tags'],
+                'class_name': 'dstc_slotfilling',
+                'threshold': 0.8,
+                'save_path': '{MODEL_PATH}/model',
+                'load_path': '{MODEL_PATH}/model',
+                'out': ['slots']
             }
         ],
-        "out": ["slots"]
+        'out': ['slots']
     },
-    "train": {
-        "metrics": ["slots_accuracy"],
-        "class_name": "fit_trainer",
-        "evaluation_targets": [
-            "valid",
-            "test"
+    'train': {
+        'metrics': ['slots_accuracy'],
+        'class_name': 'fit_trainer',
+        'evaluation_targets': [
+            'valid',
+            'test'
         ]
     },
-    "metadata": {
-        "variables": {
-            "ROOT_PATH": "~/.deeppavlov",
-            "NER_CONFIG_PATH": "{DEEPPAVLOV_PATH}/configs/ner/ner_dstc2.json",
-            "DATA_PATH": "{ROOT_PATH}/downloads/dstc2",
-            "SLOT_VALS_PATH": "{DATA_PATH}/dstc_slot_vals.json",
-            "MODELS_PATH": "{ROOT_PATH}/models",
-            "MODEL_PATH": "{MODELS_PATH}/slotfill_dstc2"
+    'metadata': {
+        'variables': {
+            'ROOT_PATH': '~/.deeppavlov',
+            'NER_CONFIG_PATH': '{DEEPPAVLOV_PATH}/configs/ner/ner_dstc2.json',
+            'DATA_PATH': '{ROOT_PATH}/downloads/dstc2',
+            'SLOT_VALS_PATH': '{DATA_PATH}/dstc_slot_vals.json',
+            'MODELS_PATH': '{ROOT_PATH}/models',
+            'MODEL_PATH': '{MODELS_PATH}/slotfill_dstc2'
         },
-        "requirements": [
-            "{DEEPPAVLOV_PATH}/requirements/tf.txt"
+        'requirements': [
+            '{DEEPPAVLOV_PATH}/requirements/tf.txt'
         ],
-        "labels": {
-            "telegram_utils": "NERModel",
-            "server_utils": "DstcSlotFillingNetwork"
+        'labels': {
+            'telegram_utils': 'NERModel',
+            'server_utils': 'DstcSlotFillingNetwork'
         },
-        "download": [
+        'download': [
             {
-                "url": "http://files.deeppavlov.ai/deeppavlov_data/dstc_slot_vals.tar.gz",
-                "subdir": "{DATA_PATH}"
+                'url': 'http://files.deeppavlov.ai/deeppavlov_data/dstc_slot_vals.tar.gz',
+                'subdir': '{DATA_PATH}'
             },
             {
-                "url": "http://files.deeppavlov.ai/deeppavlov_data/slotfill_dstc2.tar.gz",
-                "subdir": "{MODELS_PATH}"
+                'url': 'http://files.deeppavlov.ai/deeppavlov_data/slotfill_dstc2.tar.gz',
+                'subdir': '{MODELS_PATH}'
             }
         ]
     }
 }
 
 network = {
-    "in": ["x"],
-    "in_y": ["y"],
-    "out": ["y_predicted"],
-    "main": True,
-    "class_name": "go_bot",
-    "load_path": "{MODELS_PATH}/my_gobot/model",
-    "save_path": "{MODELS_PATH}/my_gobot/model",
-    "debug": False,
-    "word_vocab": "#token_vocab",
-    "template_path": "{DOWNLOADS_PATH}/dstc2/dstc2-templates.txt",
-    "template_type": "DualTemplate",
-    "api_call_action": "api_call",
-    "use_action_mask": False,
-    "network_parameters": {
-        "learning_rate": 0.005,
-        "dropout_rate": 0.5,
-        "l2_reg_coef": 7e-4,
-        "hidden_size": 128,
-        "dense_size": 160
+    'in': ['x'],
+    'in_y': ['y'],
+    'out': ['y_predicted'],
+    'main': True,
+    'class_name': 'go_bot',
+    'load_path': '{MODELS_PATH}/my_gobot/model',
+    'save_path': '{MODELS_PATH}/my_gobot/model',
+    'debug': False,
+    'word_vocab': '#token_vocab',
+    'template_path': '{DOWNLOADS_PATH}/dstc2/dstc2-templates.txt',
+    'template_type': 'DualTemplate',
+    'api_call_action': 'api_call',
+    'use_action_mask': False,
+    'network_parameters': {
+        'learning_rate': 0.005,
+        'dropout_rate': 0.5,
+        'l2_reg_coef': 7e-4,
+        'hidden_size': 128,
+        'dense_size': 160
     },
-    "slot_filler": None,
-    "intent_classifier": None,
-    "embedder": None,
-    "bow_embedder": {
-        "class_name": "bow",
-        "depth": "#token_vocab.__len__()",
-        "with_counts": True,
+    'slot_filler': None,
+    'intent_classifier': None,
+    'embedder': None,
+    'bow_embedder': {
+        'class_name': 'bow',
+        'depth': '#token_vocab.__len__()',
+        'with_counts': True,
         # },
-        # "slot_filler": {
-        "config_path": "{DEEPPAVLOV_PATH}/configs/ner/slotfill_dstc2.json"
+        # 'slot_filler': {
+        'config_path': '{DEEPPAVLOV_PATH}/configs/ner/slotfill_dstc2.json'
     },
-    "tokenizer": {
-        "class_name": "stream_spacy_tokenizer",
-        "lowercase": False
+    'tokenizer': {
+        'class_name': 'stream_spacy_tokenizer',
+        'lowercase': False
     },
-    "tracker": {
-        "class_name": "featurized_tracker",
-        "slot_names": ["pricerange", "this", "area", "food", "name"]
+    'tracker': {
+        'class_name': 'featurized_tracker',
+        'slot_names': ['pricerange', 'this', 'area', 'food', 'name']
     }
 }
 
@@ -150,6 +150,66 @@ tokenizer = {'class_name': 'deeppavlov.models.go_bot.wrapper:DialogComponentWrap
 token_vocabulary = {'class_name': 'simple_vocab', 'fit_on': ['x_tokens'], 'id': 'token_vocab',
                     'load_path': '{MODELS_PATH}/my_gobot/token.dict',
                     'save_path': '{MODELS_PATH}/my_gobot/token.dict', }
+
+basic_config = {
+    'dataset_reader': {
+        'class_name': 'dstc2_reader',
+        'data_path': '{DOWNLOADS_PATH}/dstc2'
+    },
+    'dataset_iterator': {
+        'class_name': 'my_dialog_iterator'
+    },
+    'chainer': {
+        'in': ['x'],
+        'in_y': ['y'],
+        'out': ['y_predicted'],
+        'pipe': [
+            tokenizer,
+            token_vocabulary,
+            network
+        ]
+    },
+    'train': {
+        'epochs': 200,
+        'batch_size': 4,
+
+        'metrics': ['per_item_dialog_accuracy'],
+        'validation_patience': 10,
+
+        'val_every_n_batches': 15,
+        'val_every_n_epochs': -1,
+
+        'log_every_n_batches': 15,
+        'log_every_n_epochs': -1,
+        'show_examples': False,
+        'validate_best': True,
+        'test_best': True
+    },
+    'metadata': {
+        'variables': {
+            'ROOT_PATH': '~/.deeppavlov',
+            'DOWNLOADS_PATH': '{ROOT_PATH}/downloads',
+            'MODELS_PATH': './models',
+            'CONFIGS_PATH': './configs'
+        },
+        'requirements': [
+            '{DEEPPAVLOV_PATH}/requirements/tf.txt',
+            '{DEEPPAVLOV_PATH}/requirements/fasttext.txt',
+            '{DEEPPAVLOV_PATH}/requirements/spacy.txt',
+            '{DEEPPAVLOV_PATH}/requirements/en_core_web_sm.txt'
+        ],
+        'labels': {
+            'telegram_utils': 'GoalOrientedBot',
+            'server_utils': 'GoalOrientedBot'
+        },
+        'download': [
+            {
+                'url': 'http://files.deeppavlov.ai/datasets/dstc2_v2.tar.gz',
+                'subdir': '{DOWNLOADS_PATH}/dstc2'
+            }
+        ]
+    }
+}
 
 if __name__ == '__main__':
     data = DSTC2DatasetReader().read('./dstc2')
