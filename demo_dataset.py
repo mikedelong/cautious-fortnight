9e56c4ee43fd964e9dae604ea5fbbabd9f31e8b1
mikedelong <mikedelong@outlook.com>
2019-11-07 11:18:38 -0500

    feature: a first attempt at using a dataset to build a bot

---
+++ demo_dataset.py
@@ -0,0 +1,42 @@
+from deeppavlov import build_model
+from deeppavlov import configs
+from deeppavlov.core.common.registry import register
+from deeppavlov.core.data.data_learning_iterator import DataLearningIterator
+from deeppavlov.dataset_readers.dstc2_reader import DSTC2DatasetReader
+
+
+@register('my_dialog_iterator')
+class MyDialogDatasetIterator(DataLearningIterator):
+
+    @staticmethod
+    def _dialogs(data):
+        dialogs = []
+        prev_resp_act = None
+        for x, y in data:
+            if x.get('episode_done'):
+                del x['episode_done']
+                prev_resp_act = None
+                dialogs.append(([], []))
+            x['prev_resp_act'] = prev_resp_act
+            prev_resp_act = y['act']
+            dialogs[-1][0].append(x)
+            dialogs[-1][1].append(y)
+        return dialogs
+
+    # @overrides
+    def split(self, *args, **kwargs):
+        self.train = self._dialogs(self.train)
+        self.valid = self._dialogs(self.valid)
+        self.test = self._dialogs(self.test)
+
+
+if __name__ == '__main__':
+    data = DSTC2DatasetReader().read('./dstc2')
+    iterator = MyDialogDatasetIterator(data)
+
+    x_dialog, y_dialog = iterator.train[0]
+    print('x size: {} y size: {}'.format(len(x_dialog), len(y_dialog)))
+    bot = build_model(configs.go_bot.gobot_dstc2, download=True)
+    question = 'hi i want some food'
+    answer = bot([question])
+    print('Q: {} A: {}'.format(question, answer))
