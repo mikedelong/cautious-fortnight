482b01676be15f498605098fb58123809ee9500b
mikedelong <mikedelong@outlook.com>
2019-12-02 12:03:01 -0500

    refactor: invert the if-then-else structure to put the more common branch first

--- demo_squad_bigcorpus.py
+++ demo_squad_bigcorpus.py
@@ -176,12 +176,12 @@ if __name__ == '__main__':
                 question_ = lsi[dictionary.doc2bow(question.lower().split())]
                 similarities = sorted(enumerate(matrix_similarity[question_]), key=lambda item: -item[1])[
                                :results_to_return]
-                if similarities[0][1] == 0.0:
-                    logging.info('Q: {} : lsi: {} A: {}'.format(question, 0.0, choice(miss_responses)))
-                else:
+                if similarities[0][1] != 0.0:
                     for similarity in similarities:
                         result = model([pieces[similarity[0]]], [question])
                         logging.info('Q: {} : lsi: {} A: {}'.format(question, similarity, result[0]))
+                else:
+                    logging.info('Q: {} : lsi: {} A: {}'.format(question, 0.0, choice(miss_responses)))
             else:
                 raise ValueError('mode can only be {} but is [{}]'.format(modes, mode))
         else:
