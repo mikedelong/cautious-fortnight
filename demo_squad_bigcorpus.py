9bd4be300660f5f304078a81b367341a47c181df
mikedelong <mikedelong@outlook.com>
2019-12-17 15:05:11 -0500

    feature: format the similarity

--- demo_squad_bigcorpus.py
+++ demo_squad_bigcorpus.py
@@ -244,7 +244,7 @@ if __name__ == '__main__':
                 similarities = sorted(enumerate(
                     [1.0 - cosine_similarity(question_.reshape(1, -1), piece_.reshape(1, -1), ) / pi for piece_ in
                      pieces_]), key=lambda item: -item[1])[:results_to_return]
-                d2v_format_ = 'Q: {} : d2v: {} A: {}'
+                d2v_format_ = 'Q: {} : d2v: {:5.3f} A: {}'
                 if similarities[0][1] != 0.0:
                     for similarity in similarities:
                         result = model([pieces[similarity[0]]], [question.lower()])
