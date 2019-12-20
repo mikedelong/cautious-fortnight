97176caeeee59e22228b3a5a2f9f40583a0a3601
mikedelong <mikedelong@outlook.com>
2019-12-20 12:00:10 -0500

    refactor: introduce a settings file

--- demo_wordcloud.py
+++ demo_wordcloud.py
@@ -1,20 +1,34 @@
 from collections import Counter
 from glob import glob
+from json import load as json_load
 from logging import INFO
 from logging import basicConfig
 from logging import getLogger
 from time import time
 
+import matplotlib.pyplot as plt
 from tika import parser
 from wordcloud import WordCloud
 
-input_folder = './afghanistan-papers-documents/'
-token_count = 40
 if __name__ == '__main__':
     time_start = time()
     logger = getLogger(__name__)
     basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=INFO)
     logger.info('started')
+
+    with open('./demo_wordcloud.json', 'r') as settings_fp:
+        settings = json_load(settings_fp, cls=None, object_hook=None, parse_float=None, parse_int=None,
+                             parse_constant=None, object_pairs_hook=None)
+    input_folder = settings['input_folder'] if 'input_folder' in settings.keys() else None
+    if input_folder is None:
+        logger.warning('input_folder is None. Quitting.')
+        quit(code=1)
+    stop_word = settings['stop_word'] if 'stop_word' in settings.keys() else list()
+    if not len(stop_word):
+        logger.warning('stop word list not in settings; default is empty.')
+    token_count = settings['token_count'] if 'token_count' in settings.keys() else 10
+    if 'token_count' not in settings.keys():
+        logger.warning('token_count not in settings; default value is {}.'.format(token_count))
 
     result = list()
     input_file_count = 0
@@ -27,32 +41,35 @@ if __name__ == '__main__':
     logger.info('file count: {}'.format(input_file_count))
     logger.info('result size: {}'.format(len(result)))
 
-    stop_word = ['-', '1', '2', 'a', 'about', 'all', 'also', 'an', 'and', 'are', 'as', 'at', 'be', 'because', 'been',
-                 'but', 'by', 'can', 'could', 'did', "didn't", 'do', "don't", 'for', 'from', 'get', 'going', 'good',
-                 'had', 'has', 'have', 'he', 'how', 'i', 'if', 'in', 'into', 'is', 'it', 'just', 'like', 'more', 'need',
-                 'no', 'not', 'of', 'on', 'one', 'or', 'other', 'our', 'out', 'should', 'so', 'some', 'that', 'the',
-                 'their', 'them', 'then', 'there', 'these', 'they', 'this', 'time', 'to', 'up', 'very', 'was', 'we',
-                 'were', 'what', 'when', 'which', 'who', 'will', 'with', 'work', 'would', 'you', '•', '4', '11',
-                 '--', 'go', 't', 'where', 'than', '3', 'know', 'want', 'much', 'only', '.', 'lot', 'wanted',
-                 'it.', 'got', 'make', 'things', 'back', 'through', 'over', 'doing', 'even', '5', 'any', 'his',
-                 'doing', 'even', '6', 's', 'know,', '10', 'way', 'said', 'its', 'came', 'us', 'between', 'take',
-                 'those', 'really', 'R.', 'many', 'used', 'never', 'interview', 'dr.',
-                 '7', 'after', 'it\'s', 'see', 'my', 'made', 'went', ]
-
     logger.info('stop words: {}'.format(sorted(stop_word)))
     stop_word = set(stop_word)
     count = Counter()
+    tokens_to_show = list()
     for item_index, item in enumerate(result):
         if item is not None:
             logger.info('item: {} size: {}'.format(item_index, len(item)))
             pieces = item.split()
             pieces = [piece.strip() for piece in pieces]
+            pieces = [piece if piece.endswith(':') else piece[:-1] for piece in pieces]
             for piece in pieces:
-                count[piece] += 1 if piece.lower() not in stop_word else 0
+                count[piece] += 1 if all([
+                    piece.lower() not in stop_word,
+                    not piece.isdigit(),
+                    len(piece) > 1,
+                ]) else 0
             tokens_to_show = [token[0] for token in count.most_common(n=token_count)]
             logger.info(tokens_to_show[:20])
-            logger.info(tokens_to_show[20:])
+            logger.info(tokens_to_show[20:40])
+            logger.info(tokens_to_show[40:])
         else:
             logger.info('item: {} size: 0 '.format(item_index))
+
+    to_show = {count_item[0]: count_item[1] for count_item in count.most_common(n=token_count)}
+
+    word_cloud = WordCloud().generate_from_frequencies(to_show)
+
+    plt.imshow(word_cloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
