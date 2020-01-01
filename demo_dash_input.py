c2a228a4a86308ddff89c33a4b35e0800b3e2505
mikedelong <mikedelong@outlook.com>
2020-01-01 09:40:32 -0500

    cleanup: single not double quotes

--- demo_dash_input.py
+++ demo_dash_input.py
@@ -8,30 +8,22 @@ from dash.dependencies import Output
 app = dash.Dash(__name__)
 
 ALLOWED_TYPES = (
-    "text", "number", "password", "email", "search",
-    "tel", "url", "range", "hidden",
+    'text', 'number', 'password', 'email', 'search',
+    'tel', 'url', 'range', 'hidden',
 )
 
 app.layout = html.Div(
-    [
-        dcc.Input(
-            id="input_{}".format(_),
-            type=_,
-            placeholder="input type {}".format(_),
-        )
-        for _ in ALLOWED_TYPES
-    ]
-    + [html.Div(id="out-all-types")]
-)
+    [dcc.Input(id='input_{}'.format(_), placeholder='input type {}'.format(_), type=_, ) for _ in ALLOWED_TYPES] + [
+        html.Div(id='out-all-types')])
 
 
 @app.callback(
-    Output("out-all-types", "children"),
-    [Input("input_{}".format(_), "value") for _ in ALLOWED_TYPES],
+    Output('out-all-types', 'children'),
+    [Input('input_{}'.format(_), 'value') for _ in ALLOWED_TYPES],
 )
 def cb_render(*vals):
-    return " | ".join((str(val) for val in vals if val))
+    return ' | '.join((str(val) for val in vals if val))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run_server(debug=True)
