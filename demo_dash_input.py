ef00ad07f6a03517f5576a2fd113859b4618150e
mikedelong <mikedelong@outlook.com>
2020-01-01 09:46:24 -0500

    cleanup: more whitespace cleanup, no functional changes

--- demo_dash_input.py
+++ demo_dash_input.py
@@ -7,20 +7,14 @@ from dash.dependencies import Output
 
 app = dash.Dash(__name__)
 
-ALLOWED_TYPES = (
-    'text', 'number', 'password', 'email', 'search',
-    'tel', 'url', 'range', 'hidden',
-)
+ALLOWED_TYPES = ('text', 'number', 'password', 'email', 'search', 'tel', 'url', 'range', 'hidden',)
 
 app.layout = html.Div(
     [dcc.Input(id='input_{}'.format(_), placeholder='input type {}'.format(_), type=_, ) for _ in ALLOWED_TYPES] + [
         html.Div(id='out-all-types')])
 
 
-@app.callback(
-    Output('out-all-types', 'children'),
-    [Input('input_{}'.format(_), 'value') for _ in ALLOWED_TYPES],
-)
+@app.callback(Output('out-all-types', 'children'), [Input('input_{}'.format(_), 'value') for _ in ALLOWED_TYPES], )
 def cb_render(*vals):
     return ' | '.join((str(val) for val in vals if val))
 
