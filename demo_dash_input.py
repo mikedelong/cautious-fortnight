29f6b16c57ecddaa8852667f88bd3c97a743dcb6
mikedelong <mikedelong@outlook.com>
2020-01-02 11:49:38 -0500

    refactor: inline a variable used only once

--- demo_dash_input.py
+++ demo_dash_input.py
@@ -3,9 +3,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 app.layout = html.Div([
     html.Div(dcc.Input(id='input-box', type='text')),
     html.Button('Submit', id='button'),
