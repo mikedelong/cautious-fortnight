3ee343892c4ed21c2f5993c9c122510bc19968f3
mikedelong <mikedelong@outlook.com>
2020-01-14 08:55:01 -0500

    refactor: for the moment, separate creating the figure from returning it as a result, so we can do something about the axes ticks

--- demo_updating_wordcloud.py
+++ demo_updating_wordcloud.py
@@ -68,13 +68,15 @@ def update_graph_live(n):
     min_size = min(this[1] for this in word_cloud.layout_)
 
     # todo turn off the axes tick marks
-    return Figure(data=[Scatter(mode='text', text=[this[0][0] for this in word_cloud.layout_],
-                                x=[this[2][0] for this in word_cloud.layout_],
-                                y=[this[2][1] for this in word_cloud.layout_], textfont=dict(
+    result = Figure(data=[Scatter(mode='text', text=[this[0][0] for this in word_cloud.layout_],
+                                  x=[this[2][0] for this in word_cloud.layout_],
+                                  y=[this[2][1] for this in word_cloud.layout_], textfont=dict(
             color=[float_color_to_hex(int((this[1] - min_size) * 255 / max_size), colormap) for this in
                    # todo make the sizes less disparate
                    word_cloud.layout_], size=[2 * this[1] for this in word_cloud.layout_], ))],
-                  layout=Layout(autosize=True, height=800, width=1800, ))
+                    layout=Layout(autosize=True, height=800, width=1800, ))
+
+    return result
 
 
 if __name__ == '__main__':
