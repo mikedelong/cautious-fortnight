33c7c8c9693f2431e3486de939714a47c3ecffbf
mikedelong <mikedelong@outlook.com>
2020-01-17 10:55:56 -0500

    bug fix: a second attempt at adding the counts to the hover text

--- demo_updating_wordcloud.py
+++ demo_updating_wordcloud.py
@@ -63,13 +63,13 @@ def update_graph_live(n):
     min_size = min(this[1] for this in word_cloud.layout_)
 
     return Figure(data=[Scatter(mode='text', text=[this[0][0] for this in word_cloud.layout_],
+                                hovertext=[this[1] for this in word_cloud.layout_],
                                 x=[this[2][0] for this in word_cloud.layout_],
                                 y=[this[2][1] for this in word_cloud.layout_], textfont=dict(
             # todo add the count for each token to the hover text
             # todo make the sizes less disparate
             color=[float_color_to_hex(int((this[1] - min_size) * 255 / max_size), colormap) for this in
                    word_cloud.layout_],
-            hovertext=[this[1] for this in word_cloud.layout_],
             size=[2 * this[1] for this in word_cloud.layout_], ))],
                   layout=Layout(autosize=True, height=800, width=1800, xaxis=dict(showticklabels=False),
                                 yaxis=dict(showticklabels=False), ))
