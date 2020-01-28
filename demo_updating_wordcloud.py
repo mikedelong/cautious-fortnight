277196a6d0e0cc54d552823e2a226e26d075cd93
mikedelong <mikedelong@outlook.com>
2020-01-28 08:54:36 -0500

    cleanup: remove code that reads the plurals setting

--- demo_updating_wordcloud.py
+++ demo_updating_wordcloud.py
@@ -102,13 +102,6 @@ if __name__ == '__main__':
         logger.info('plotly/HTML colormap: {}'.format(plotly_colormap))
     else:
         logger.warning('plotly/HTML colormap not in settings; using default: {}'.format(plotly_colormap))
-    plurals = settings['plurals'] if 'plurals' in settings.keys() else dict()
-    if len(plurals):
-        with open(settings['plurals'], 'r') as plurals_fp:
-            plurals = json_load(plurals_fp)
-            logger.info('plurals: {}'.format(plurals))
-    else:
-        logger.warning('plurals not in settings; we will not be doing any singular/plural reconciliation')
     stop_word = settings['stop_word'] if 'stop_word' in settings.keys() else list()
     if len(stop_word):
         logger.info('stop word list: {}'.format(stop_word))
