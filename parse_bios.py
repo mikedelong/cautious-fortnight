6071c171581291907db22169994612d91735607f
mikedelong <mikedelong@outlook.com>
2020-02-19 11:35:59 -0500

    feature: a first cut at extracting the net text, after we have removed the common tokens; we still need to remove the markup

--- parse_bios.py
+++ parse_bios.py
@@ -59,6 +59,7 @@ if __name__ == '__main__':
     # next get the set of distinct tokens by flattening
     tokens = set([item for sublist in all_text for item in sublist])
     common = {item for item in tokens if all([item in piece for piece in all_text])}
+    net = [[item for item in sublist if item not in common] for sublist in all_text]
 
     logger.info('found/missing: {}/{}'.format(found, missing))
     logger.info('input file count: {}'.format(len(input_files)))
