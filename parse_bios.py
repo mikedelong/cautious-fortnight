3cc75f926a3311f7a91c01baabcd70707e2a600d
mikedelong <mikedelong@outlook.com>
2020-02-24 10:42:55 -0500

    refactor: inline the t-sne plot function

--- parse_bios.py
+++ parse_bios.py
@@ -26,33 +26,6 @@ def process(arg):
     return None if str(this.text) == 'Download Official Photo' or str(this.text).startswith('Updated') else this.text
 
 
-def tsne_plot(model):
-    labels = []
-    tokens = []
-
-    for word in model.wv.vocab:
-        tokens.append(model[word])
-        labels.append(word)
-
-    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
-    new_values = tsne_model.fit_transform(tokens)
-
-    x = []
-    y = []
-    for value in new_values:
-        x.append(value[0])
-        y.append(value[1])
-
-    plt.figure(figsize=(16, 16))
-    for i in range(len(x)):
-        plt.scatter(x[i], y[i])
-        plt.annotate(labels[i],
-                     xy=(x[i], y[i]),
-                     xytext=(5, 2),
-                     textcoords='offset points',
-                     ha='right',
-                     va='bottom', )
-    plt.show()
 
 
 if __name__ == '__main__':
@@ -116,7 +89,32 @@ if __name__ == '__main__':
 
     model = Word2Vec(corpus, size=100, window=20, min_count=200, workers=4, )
     logger.info(model.wv['officer'])
-    tsne_plot(model=model)
+    labels = []
+    tokens = []
+
+    for word in model.wv.vocab:
+        tokens.append(model[word])
+        labels.append(word)
+
+    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(tokens)
+
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+
+    plt.figure(figsize=(16, 16))
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.annotate(labels[i],
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom', )
+    plt.show()
 
     logger.info('found/missing: {}/{}'.format(found, missing))
     logger.info('input file count: {}'.format(len(input_files)))
