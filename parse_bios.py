630d50142b9f14dc546202d7bf58058299c7d3c7
mikedelong <mikedelong@outlook.com>
2020-02-24 10:40:03 -0500

    feature: first working version of word2vec/TSNE visualization

--- parse_bios.py
+++ parse_bios.py
@@ -1,3 +1,4 @@
+# word2vec/T-SNE visualization modified from https://www.kaggle.com/jeffd23/visualizing-word-vectors-with-t-sne
 from glob import glob
 from json import load as json_load
 from logging import INFO
@@ -5,8 +6,11 @@ from logging import basicConfig
 from logging import getLogger
 from time import time
 
+import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
+from gensim.models import Word2Vec
 from lxml import html
+from sklearn.manifold import TSNE
 
 
 def process(arg):
@@ -20,6 +24,35 @@ def process(arg):
         return result if result != 'Navy Biography' else None
 
     return None if str(this.text) == 'Download Official Photo' or str(this.text).startswith('Updated') else this.text
+
+
+def tsne_plot(model):
+    labels = []
+    tokens = []
+
+    for word in model.wv.vocab:
+        tokens.append(model[word])
+        labels.append(word)
+
+    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(tokens)
+
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+
+    plt.figure(figsize=(16, 16))
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.annotate(labels[i],
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom', )
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -79,6 +112,11 @@ if __name__ == '__main__':
     net = [[process(item) for item in sublist] for sublist in net]
     net = [[item for item in sublist if item is not None] for sublist in net]
     lengths = [len(item) for item in net]
+    corpus = [' '.join([item for item in sublist]).split() for sublist in net]
+
+    model = Word2Vec(corpus, size=100, window=20, min_count=200, workers=4, )
+    logger.info(model.wv['officer'])
+    tsne_plot(model=model)
 
     logger.info('found/missing: {}/{}'.format(found, missing))
     logger.info('input file count: {}'.format(len(input_files)))
