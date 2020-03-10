3c721acd303778a5016dd2ecf9f03d2478143ad0
mikedelong <mikedelong@outlook.com>
2020-03-10 10:26:26 -0400

    feature: another abbreviation merge

--- pdf_folder_to_word2vec.py
+++ pdf_folder_to_word2vec.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
              'Mar': ['March'], 'Jun': ['June'], 'Jul': ['July'], 'Aug': ['August'], 'Sep': ['September'],
              'Oct': ['October'], 'Nov': ['November'], 'Dec': ['December'], 'budgetconsultation':
                  ['budget', 'consultation'], 'Msg': ['message'], 'msg': ['message'], 'Diem\'s': ['Diem'],
-             'Viet-Nam': ['Vietnam'], 'Vietnem': ['Vietnam']}
+             'Viet-Nam': ['Vietnam'], 'Vietnem': ['Vietnam'], 'deg': ['degree'], }
     # todo factor these out as data
     join = {'Accord-', 'Act-', 'accept-', 'accord-', 'act-', 'arm-', 'attach-', 'attack-', 'back-', 'bargain-',
             'belong-', 'bind-', 'boil-', 'bomb', 'bomb-', 'bowl', 'carry-', 'choos-', 'command-', 'compromis-',
