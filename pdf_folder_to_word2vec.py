d13608fa8b4132a27dcc85cb8ede87305234b01f
mikedelong <mikedelong@outlook.com>
2020-03-10 09:17:56 -0400

    feature: more -ing split fixes

--- pdf_folder_to_word2vec.py
+++ pdf_folder_to_word2vec.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
             'provid-', 'question-', 'reach-', 'reconven-', 'regard-', 'report-', 'return-', 'secur-', 'seek-', 'start-',
             'stress-', 'strik-', 'support-', 'tak-', 'train-', 'understand-', 'visit-', 'warn-', 'will-', 'work-',
             'lead-', 'demonstrat-', 'bring-', 'involv-', 'operat-', 'encourag-', 'help-', 'resist-', 'expand-',
-            'regroup-', 'achiev-'}
+            'regroup-', 'achiev-', 'unyield-', 'open-', 'try-', 'forward-', 'speak-', 'show-', }
     logger.info('join data: {}'.format(sorted(list(join))))
     # todo factor these out as data
     joined = {'According', 'Acting', 'accepting', 'according', 'acting', 'arming', 'attaching', 'attacking', 'backing',
@@ -117,7 +117,8 @@ if __name__ == '__main__':
               'proposing', 'providing', 'questioning', 'reaching', 'reconvening', 'regarding', 'reporting', 'returning',
               'securing', 'seeking', 'starting', 'stressing', 'striking', 'supporting', 'taking', 'training',
               'understanding', 'visiting', 'warning', 'willing', 'working', 'leading', 'demonstrating', 'involving',
-              'operating', 'encouraging', 'helping', 'resisting', 'expanding', 'regrouping', 'achieving', }
+              'operating', 'encouraging', 'helping', 'resisting', 'expanding', 'regrouping', 'achieving', 'bringing',
+              'unyielding', 'opening', 'trying', 'forwarding', 'speaking', 'showing', }
     logger.info('joined data: {}'.format(sorted(list(joined))))
 
     ing_counts = Counter()
