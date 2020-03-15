f787deb453689bfd33711acd18236f104e6fbb21
mikedelong <mikedelong@outlook.com>
2020-03-15 15:35:49 -0400

    feature: more join fixes

--- pdf_folder_to_word2vec.py
+++ pdf_folder_to_word2vec.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
               'resisting', 'resulting', 'returning', 'securing', 'seeking', 'selective', 'sending', 'sensitive',
               'showing', 'speaking', 'starting', 'stressing', 'striking', 'supporting', 'surrounding', 'taking',
               'training', 'trying', 'understanding', 'unyielding', 'urging', 'visiting', 'warning', 'weakening',
-              'willing', 'working', }
+              'willing', 'working', "attractive", "respective", "restrictive", "thinking", }
     logger.info('joined data: {}'.format(sorted(list(joined))))
 
     ing_counts = Counter()
