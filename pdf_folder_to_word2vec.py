dc7ff82f61644018629f5a3c0063e0fb9781c3a6
mikedelong <mikedelong@outlook.com>
2020-03-18 11:06:01 -0400

    feature: switch to cosine distance

--- pdf_folder_to_word2vec.py
+++ pdf_folder_to_word2vec.py
@@ -191,7 +191,7 @@ if __name__ == '__main__':
     tsne_init = ['pca', 'random'][0]
     tsne_learning_rate = 1000.0
     tsne_method = ['barnes_hut', 'exact'][0]
-    tsne_metric = ['cosine', 'euclidean'][1]
+    tsne_metric = ['cosine', 'euclidean'][0]
     tsne_model = TSNE(angle=tsne_angle, early_exaggeration=tsne_early_exaggeration, init=tsne_init,
                       learning_rate=tsne_learning_rate, method=tsne_method, metric=tsne_metric,
                       min_grad_norm=min_grad_norm, n_components=2, n_iter=n_iter, n_iter_without_progress=300,
